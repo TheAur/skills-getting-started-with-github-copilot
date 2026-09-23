1b0247af10a5e5073e01c4e09eee8227b48f9385
Павел <fariseevp@gmail.com>
2026-09-23 21:21:46 +0000

    Добавлено удаление участников из мероприятий и соответствующий тест

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent / "src"))
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(PROJECT_ROOT / "src"))
 
 from fastapi.testclient import TestClient
 
